0614e2e123838b8b579743cf8b049ce0d77f475a
ajengrf <ajengrf.12@gmail.com>
2019-12-22 00:02:17 +0700

    done

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
-const whiteKeyboard = ['z', 'x', 'c', 'v', 'b', 'n', 'm', ',']
-const blackKeyboard = ['s', 'd', 'g', 'h', 'j']
-
 const keys = document.querySelectorAll('.key')
 const whiteKeys = document.querySelectorAll('.key.white')
 const blackKeys = document.querySelectorAll('.key.black')
 
-keys.forEach(key => {
-    key.addEventListener('click', () => playNote(key))
+keys.forEach(function (clicked) {
+    clicked.addEventListener('click', function () {
+        playNote(clicked)
+    })
 })
 
-document.addEventListener('keydown', e => {
-    if (e.repeat) {
-        return
-    }
-
-    const key = e.key
-    const whiteKeyIndex = whiteKeyboard.indexOf(key)
-    const blackKeyIndex = blackKeyboard.indexOf(key)
-
-    if (whiteKeyIndex > -1) {
-        playNote(whiteKeys[whiteKeyIndex])
-    }
-    if (blackKeyIndex > -1) {
-        playNote(blackKeys[blackKeyIndex])
-    }
-})
-
-function playNote(key) {
-    const noteAudio = document.getElementById(key.dataset.note)
+function playNote(clicked) {
+    const noteAudio = document.getElementById(clicked.dataset.note) // ngakses value -> element.dataset.keyname // karena di HTMLnya udah urutan
     noteAudio.currentTime = 0
     noteAudio.play()
-    key.classList.add('active')
-    noteAudio.addEventListener('ended', () => {
-        key.classList.remove('active')
-    })
+    clicked.classList.add('active') // seolah-olah hover
+
+    noteAudio.addEventListener('ended', stop) //berhenti setelah audio selesai
+    function stop() {
+        clicked.classList.remove('active')
+    }
 }
+
+document.addEventListener('keydown', function(event) {
+    const whiteKeyboard = ['z', 'x', 'c', 'v', 'b', 'n', 'm', ',']
+    const blackKeyboard = ['s', 'd', 'g', 'h', 'j']
+
+    const pressKey = event.key // keyboard yang ditekan
+
+    const pressWhiteKeyboard = whiteKeyboard.indexOf(pressKey) // outputnya number of index di whiteKeyboard
+    const pressBlackKeyboard = blackKeyboard.indexOf(pressKey)
+    
+    if (event.repeat) { // mencegah kalo diteken lama bunyi terus
+        return
+    }
+    if (pressWhiteKeyboard > -1) { // karna indexnya mulai dari 0
+        playNote(whiteKeys[pressWhiteKeyboard])
+    }
+    if (pressBlackKeyboard > -1) {
+        playNote(blackKeys[pressBlackKeyboard])
+    }
+})
